22bff5b4a7397b01a7365bf8d0e7510b4a8173af
Angel Montes <angelmontes963@gmail.com>
2024-11-28 19:35:39 -0600

    Se ve el tema de operadores ternarios

---
+++ 02-intro-javascript/src/bases/12.-operadores.js
@@ -0,0 +1,11 @@
+/*****************************/
+// Operador condicional ternario
+/*****************************/
+
+const activo = true;
+
+let mensaje;
+
+// mensaje = (activo) ? 'Activo' : null;
+
+console.log(mensaje);
